9bab07492faa1717495624e22dfaf16b44c462ed
Oleg Grushetskyy <olegfkl@gmail.com>
2017-11-20 15:10:06 +0000

    First jasmine test

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,14 @@
+// describe("FizzBuzz", function() {
+//
+//
+// it("Should re")
+//
+//
+// });
+
+
+describe("fizzBuzz", function() {
+  it("Should Fizz when passed a number multiple of 3", function() {
+    expect(fizzbuzz(3)).toEqual('Fizz');
+  });
+});
